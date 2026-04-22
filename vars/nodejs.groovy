d46a560a9ee15424f9ba0ebe97ce183a6071be75
Mukesh.Sahu <msahu201989@gmail.com>
2026-04-22 13:20:05 +0530

    commit

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -4,7 +4,7 @@ def call(){
       if (! env.TAG_NAME){
           env.TAG_NAME = ""
       }
-         sh 'env'
+        bat 'set'
          common.codeQuality()
 
          if ( BRANCH_NAME == "main" || TAG_NAME ==~ ".*" ) {
