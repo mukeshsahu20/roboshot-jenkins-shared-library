caa1616bdf9b0ec16ec01817a205f95b2457569e
Mukesh.Sahu <msahu201989@gmail.com>
2026-04-22 18:17:13 +0530

    commit

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,54 +1,42 @@
-def call(){
-    node {
+pipeline{
 
-      if (! env.TAG_NAME){
-          env.TAG_NAME = ""
-      }
-        bat 'set'
-         common.codeQuality()
+    agent any
 
-         if ( BRANCH_NAME == "main" || TAG_NAME ==~ ".*" ) {
-             stage('style checks') {
-               echo 'style checks'
-             }
-         }
+ stage('unit tests') {
+                when {
+                    anyOf {
+                        branch 'main'
+                        tag "*"
+                    }
+                }
 
-////
-//            stage('unit tests') {
-//                when {
-//                    anyOf {
-//                        branch 'main'
-//                        tag "*"
-//                    }
-//                }
-//
-//                steps {
-//                    echo "unit tests"
-//                }
-//            }
-//
-//            stage('Download dependencies') {
-//                when { tag "*" }
-//                steps {
-//                    echo "Download dependencies only when there is Tag"
-//                }
-//            }
-//
-//            stage('Prepare artifact') {
-//                when { tag "*" }
-//                steps {
-//                    echo "Prepare artifact only when tag is there"
-//                }
-//            }
-//
-//            stage('Publish artifact') {
-//                when { tag "*" }
-//                steps {
-//                    echo "Publish artifact only when there is Tag"
-//                }
-//            }
+                steps {
+                    echo "unit tests"
+                }
+            }
+
+            stage('Download dependencies') {
+                when { tag "*" }
+                steps {
+                    echo "Download dependencies only when there is Tag"
+                }
+            }
+
+            stage('Prepare artifact') {
+                when { tag "*" }
+                steps {
+                    echo "Prepare artifact only when tag is there"
+                }
+            }
+
+            stage('Publish artifact') {
+                when { tag "*" }
+                steps {
+                    echo "Publish artifact only when there is Tag"
+                }
+            }
 
         }
 
-    }
+
 
