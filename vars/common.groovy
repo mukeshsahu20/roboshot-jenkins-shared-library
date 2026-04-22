66a319750016c39b02a97341ca0e63439a1a4f5e
Mukesh.Sahu <msahu201989@gmail.com>
2026-04-22 20:26:32 +0530

    commit

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,6 +1,34 @@
 def codeQuality() {
-
     stage('code quality') {
             echo "code quality"
-        }
+     }
+}
+
+def Codechecks () {
+    if (BRANCH_NAME == "main" || TAG_NAME ==~ ".*") {
+        stage('style checks') {
+        echo 'style checks'
     }
+
+       stage('Unit Tests') {
+          echo 'Unit Test'
+        }
+  }
+}
+
+def artifacts() {
+    if (TAG_NAME ==~ ".*"){
+
+        stage ('Downoad Dependencies'){
+            echo 'Download dependencies'
+        }
+
+    stage ('Prepare Artifacts'){
+        echo 'Prepare Artifacts'
+    }
+
+    stage ('Publish Artifacts'){
+      echo 'publish Artifacts'
+        }
+     }
+}
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -4,49 +4,14 @@ def call(){
       if (! env.TAG_NAME){
           env.TAG_NAME = ""
       }
-        bat 'set'
+
          common.codeQuality()
+         common.stylechecks()
+         common.artifacts()
 
-         if ( BRANCH_NAME == "main" || TAG_NAME ==~ ".*" ) {
-             stage('style checks') {
-               echo 'style checks'
-             }
-         }
 
-////
-//            stage('unit tests') {
-//                when {
-//                    anyOf {
-//                        branch 'main'
-//                        tag "*"
-//                    }
-//                }
-//
-//                steps {
-//                    echo "unit tests"
-//                }
-//            }
-//
-//            stage('Download dependencies') {
-//                when { tag "*" }
-//                steps {
-//                    echo "Download dependencies only when there is Tag"
-//                }
-//            }
-//
-//            stage('Prepare artifact') {
-//                when { tag "*" }
-//                steps {
-//                    echo "Prepare artifact only when tag is there"
-//                }
-//            }
-//
-//            stage('Publish artifact') {
-//                when { tag "*" }
-//                steps {
-//                    echo "Publish artifact only when there is Tag"
-//                }
-//            }
+
+
 
         }
 
