3ddc9f7d56f118bfcb90b88774be9f8b6df90212
Mukesh.Sahu <msahu201989@gmail.com>
2026-04-24 19:55:07 +0530

    commit

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,16 +1,22 @@
 def call(){
     node {
+        try {
+
+            common.codeQuality()
+            common.codechecks()
+            common.artifacts()
+        } catch (Exceptions e) {
+      echo "mail bcc: '', body: '', cc: '', from: 'mukeshsahu20@gmail.com', replyTo: '', subject: 'Pipeline job failed', to: 'mukeshsahu20@gmail.com'"
+        }
 
 //      if (! env.TAG_NAME){
 //          env.TAG_NAME = ""
 //      }
 
-         common.codeQuality()
-         common.codechecks()
-         common.artifacts()
+
 
 
         }
+       }
 
-    }
 
